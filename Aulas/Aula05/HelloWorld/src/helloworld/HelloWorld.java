35bf654a96912ad1f17383eee8515c0dbd766c10
Lucas Gabriel Theodoro <lucas.g.theodooro@gmail.com>
2022-06-26 16:05:03 -0300

    Colocando a Aula05 com os Exer
    Ta com a explicação da aula05 e com os exercício corrigido

---
+++ Aulas/Aula05/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+
+package helloworld;
+
+public class HelloWorld {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hello,Word");
+        /*
+        ° "  public static  ", é um modificador de acesso do método;
+        ° "  void  ", Tipo de retorno do método, ele não retorna nada, então por isso ele é void
+        ° "  main  ", seria para executar o código que está dentro das chaves;
+        ° "  out  ", vai sair para a tela, vamos conseguir ver o "Hello, Word";
+        ° "  (String[] args)  ",  é Parâmetros/argumentos do Método
+        --> O Java é Case Sensitive, ele se escrever algo com uma letra maior do que realmente deveria, ele não vai entender 
+        
+        ° "System.out.println('Hello Word')", é o código
+        */
+        
+    }
+}
