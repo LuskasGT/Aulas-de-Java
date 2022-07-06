3290fb661f09d538d2256e5f809ceaeed66fdb52
Lucas Gabriel Theodoro <lucas.g.theodooro@gmail.com>
2022-07-06 08:22:24 -0300

    Adicionando a Aula10
    Entrando com as variáveis

---
+++ Aulas/Aula10/Variaveis/src/variaveis/Variaveis.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variaveis;
+
+/*O que são variáveis?
+É Área de memória associada a um nome, que pode armazenar valores de um determinado tipo
+Exemplo: armazenar idade de uma pessoa ou o nome de alguém
+
+PARA CRIAR UMA VARIAVEL
+
+<Tipo> <Nome da variável>;
+<Tipo> <Nome da variável> = <valor>;
+*/
+
+public class Variaveis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 20;
+        String nome = "Lucas";
+        String nomeDoMeuCachorro = "Adolf";
+        
+        System.out.println("A minha idade é "+idade);
+        System.out.println("O meu nome é "+nome);
+        
+        
+    }   
+}
