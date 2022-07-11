820da10fc869cf86721e95e7f5d0309bdf5a30ff
Lucas Gabriel Theodoro <lucas.g.theodooro@gmail.com>
2022-07-11 10:06:22 -0300

    Aula13

---
+++ Aulas/Aula13/Operadores/src/operadores/Operadores.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadores;
+
+/**
+ *
+ * @author lucas
+ */
+public class Operadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+        
+        resultado = resultado - 1;
+        System.out.println(resultado);
+         
+        resultado = resultado * 2;
+        System.out.println(resultado);
+        
+        resultado = resultado / 2;
+        System.out.println(resultado);
+        
+        resultado = resultado + 8;
+        System.out.println(resultado);
+        
+        resultado = resultado % 7;
+        System.out.println(resultado);
+        
+        String primeiroNome = "Esta é ";
+        String segundoNome = "uma String concatenada";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println (terceiroNome);
+        
+        //resultado final é 4
+        resultado = resultado + 1;
+        System.out.println(resultado);
+        
+        
+        
+        
+        //vai ficar com 5
+        System.out.println(resultado++);
+        // com o ++ na frente da variavel, seria a mesma coisa que 
+        //System.out.println(resultado);
+        //resultado = resultado + 1;
+        //resultado +=1;
+                                                            //só muda a forma que vai ser construido
+    
+        // vai ficar com 7
+        System.out.println(++resultado);
+        //com o ++ antes da variavel, seria a mesma coisa que 
+        //resultado +=1
+        //System.out.println(resultado)
+        
+        
+        resultado--;
+        System.out.println(resultado);
+        
+        System.out.println(resultado--);
+        System.out.println(--resultado);
+        
+        
+        
+        
+        
+        
+        
+        //Operadores Logicos, daqui em diante
+        
+        int valor1 = 1;
+        int valor2 = 2;
+        
+        System.out.println("valor1 == valor2: " + (valor1 == valor2));
+        System.out.println("valor1 != valor2: " + (valor1 != valor2));
+        System.out.println("valor1 > valor2: " + (valor1 > valor2));
+        System.out.println("valor1 < valor2: " + (valor1 < valor2));
+        System.out.println("valor1 >= valor2: " + (valor1 >= valor2));
+        System.out.println("valor1 <= valor2: " + (valor1 <= valor2));
+    }
+
+}
